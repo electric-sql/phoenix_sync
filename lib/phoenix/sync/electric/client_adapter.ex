67cf5f684d2daeaf1705573eae4c76c743dd9c2e
James Arthur <thruflo@gmail.com>
2025-06-21 20:25:11 +0200

    client_adapter: make upstream fetch requests interruptible.

--- lib/phoenix/sync/electric/client_adapter.ex
+++ lib/phoenix/sync/electric/client_adapter.ex
@@ -7,6 +7,7 @@ defmodule Phoenix.Sync.Electric.ClientAdapter do
     alias Electric.Client
 
     alias Phoenix.Sync.PredefinedShape
+    alias Phoenix.Sync.ShapeRegistry
 
     def predefined_shape(sync_client, %PredefinedShape{} = predefined_shape) do
       shape_client = PredefinedShape.client(sync_client.client, predefined_shape)
@@ -46,11 +47,38 @@ defmodule Phoenix.Sync.Electric.ClientAdapter do
     defp normalise_method(method), do: method |> String.downcase() |> String.to_atom()
     defp live?(live), do: live == "true"
 
-    defp fetch_upstream(sync_client, conn, request) do
+    # Skip interruptibility for initial requests.
+    defp fetch_upstream(%{client: client, shape_definition: nil}, conn, request) do
       response =
-        case Electric.Client.Fetch.request(sync_client.client, request) do
-          {:error, response} -> response
-          response -> response
+        case Client.Fetch.request(client, request) do
+          %Client.Fetch.Response{} = response ->
+            response
+
+          {:error, %Client.Fetch.Response{} = response} ->
+            response
+        end
+
+      conn
+      |> put_headers(response.headers)
+      |> Plug.Conn.send_resp(response.status, response.body)
+    end
+
+    defp fetch_upstream(%{shape_definition: shape} = sync_client, conn, request) do
+      key = Base.encode64(:crypto.strong_rand_bytes(18))
+
+      ShapeRegistry.register_shape(key, shape)
+
+      response =
+        try do
+          case interruptible_fetch(sync_client, request, key) do
+            %Client.Fetch.Response{} = response ->
+              response
+
+            {:error, %Client.Fetch.Response{} = response} ->
+              response
+          end
+        after
+          ShapeRegistry.unregister_shape(key)
         end
 
       conn
@@ -66,6 +94,57 @@ defmodule Phoenix.Sync.Electric.ClientAdapter do
           Plug.Conn.put_resp_header(conn, header, value)
         end)
       end)
+    end
+
+    defp interruptible_fetch(%{client: client}, request, key) do
+      task =
+        Task.async(fn ->
+          Client.Fetch.request(client, request)
+        end)
+
+      ref = Process.monitor(task.pid)
+
+      # Wait for an interrupt, or the task to finish.
+      receive do
+        {:interrupt_shape, ^key, :server_interrupt} ->
+          Task.shutdown(task, :brutal_kill)
+          Process.demonitor(ref, [:flush])
+
+          interruption_response(request)
+
+        {:DOWN, ^ref, :process, _pid, :normal} ->
+          # Task was successful, return the result.
+          Task.await(task, 0)
+
+        {:DOWN, ^ref, :process, _pid, reason} ->
+          # Task failed, return an error tuple. If not handled,
+          # this will result in a 500 response.
+          {:error, reason}
+      end
+    end
+
+    # Construct the response to return with when interrupted.
+    # We want this to cause the client to reconnect to the same URL.
+    # Ideally we want to avoid putting 500s in the console logs.
+    defp interruption_response(%Client.Fetch.Request{params: params}) do
+      %Client.Fetch.Response{
+        status: 200,
+        headers: %{
+          # Construct a non-cachable response to make the client reconnect again
+          # on the same URL. This avoids guessing the cache config. The subsequent
+          # response will replace the response in the shared and browser cache.
+          "cache-control" => "no-cache, no-store, must-revalidate, max-age=0",
+          "content-type" => "application/json",
+          "electric-cursor" => Map.get(params, "cursor", nil),
+          "electric-handle" => Map.fetch!(params, "handle"),
+          "electric-offset" => Map.fetch!(params, "offset"),
+          "expires" => "0",
+          "pragma" => "no-cache",
+          "sync-interrupted" => "true"
+        },
+        body: [],
+        request_timestamp: DateTime.utc_now()
+      }
     end
   end
 end
--- lib/phoenix/sync/shape_registry.ex
+++ lib/phoenix/sync/shape_registry.ex
@@ -5,6 +5,7 @@ defmodule Phoenix.Sync.ShapeRegistry do
   use GenServer
 
   alias Electric.Client.ShapeDefinition
+  alias Phoenix.Sync.PredefinedShape
 
   # Client API
 
@@ -12,11 +13,21 @@ defmodule Phoenix.Sync.ShapeRegistry do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  def register_shape(key, %ShapeDefinition{} = shape) when is_binary(key) do
+  def register_shape(key, %ShapeDefinition{} = shape_definition) when is_binary(key) do
+    register_shape(key, PredefinedShape.from_shape_definition(shape_definition))
+  end
+
+  def register_shape(key, %PredefinedShape{query: _querable, relation: nil} = shape)
+      when is_binary(key) do
+    register_shape(key, PredefinedShape.from_queryable!(shape))
+  end
+
+  def register_shape(key, %PredefinedShape{relation: {_namespace, _table}} = shape)
+      when is_binary(key) do
     GenServer.call(__MODULE__, {:register, key, shape})
   end
 
-  def register_shape(key) when is_binary(key) do
+  def unregister_shape(key) when is_binary(key) do
     GenServer.call(__MODULE__, {:unregister, key})
   end
 
@@ -36,14 +47,14 @@ defmodule Phoenix.Sync.ShapeRegistry do
   end
 
   def interrupt_matching(table, nil) when is_binary(table) do
-    interrupt_matching(fn %ShapeDefinition{table: shape_table} ->
-      shape_table === table
+    interrupt_matching(fn %PredefinedShape{relation: {_, shape_table}} ->
+      shape_table == table
     end)
   end
 
   def interrupt_matching(table, namespace) when is_binary(table) and is_binary(namespace) do
-    interrupt_matching(fn %ShapeDefinition{table: shape_table, namespace: shape_namespace} ->
-      table === shape_table and namespace === shape_namespace
+    interrupt_matching(fn %PredefinedShape{relation: relation} ->
+      relation == {namespace, table}
     end)
   end
 
@@ -53,25 +64,30 @@ defmodule Phoenix.Sync.ShapeRegistry do
     {:ok, %{subscriptions: %{}, monitors: %{}}}
   end
 
-  def handle_call({:register, key, shape}, _from, state) do
-    # Monitor the request process
-    monitor_ref = Process.monitor(shape.request_pid)
+  def handle_call(
+        {:register, key, shape},
+        {request_pid, _ref},
+        %{monitors: monitors, subscriptions: subscriptions} = state
+      ) do
+    monitor_ref = Process.monitor(request_pid)
+    monitors = Map.put(monitors, monitor_ref, key)
 
-    state = %{
-      state
-      | subscriptions: Map.put(state.subscriptions, key, shape),
-        monitors: Map.put(state.monitors, monitor_ref, key)
-    }
+    shape_info = {shape, request_pid}
+    subscriptions = Map.put(subscriptions, key, shape_info)
 
-    {:reply, :ok, state}
+    {:reply, :ok, %{state | monitors: monitors, subscriptions: subscriptions}}
+  end
+
+  def handle_call({:unregister, key}, _from, %{subscriptions: subscriptions} = state) do
+    {:reply, :ok, %{state | subscriptions: Map.delete(subscriptions, key)}}
   end
 
   def handle_call({:interrupt_matching, matcher}, _from, state) do
     interrupted_count =
       state.subscriptions
-      |> Enum.filter(fn {_id, shape} -> matcher.(shape) end)
-      |> Enum.reduce(0, fn {key, shape}, acc ->
-        send(shape.request_pid, {:interrupt_shape, key, :server_interrupt})
+      |> Enum.filter(fn {_id, {shape, _request_pid}} -> matcher.(shape) end)
+      |> Enum.reduce(0, fn {key, {_shape, request_pid}}, acc ->
+        send(request_pid, {:interrupt_shape, key, :server_interrupt})
 
         acc + 1
       end)
